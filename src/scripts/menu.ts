f6f5c65a4ab88e375c88745982e94d1e5dc3e111
Juan <Jinbyos@Gmail.com>
2024-11-21 20:13:45 -0300

    Added new post, responsiveness, styles, tag page and posts filters, rss feed

---
+++ src/scripts/menu.ts
@@ -0,0 +1,9 @@
+document.querySelector('.hamburger')?.addEventListener('click', () => {
+    const navLinks = document.querySelector(".nav-links")
+    navLinks?.classList.toggle('expanded');
+    const isExpanded = navLinks?.classList.contains('expanded')
+    
+    document.querySelectorAll('.hamburger .line').forEach(line => {
+        (line as HTMLElement).style.marginBottom = isExpanded ? '2px' : '10px';
+    })
+});
