698526fe4cf189ad844f5fcab7cbcf1872f98782
Juan <Jinbyos@Gmail.com>
2024-11-23 20:08:44 -0300

    Added transitions and fixed theme change scripts

--- src/components/ThemeIconIsland.jsx
+++ src/components/ThemeIconIsland.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "preact/hooks";
+import { useEffect } from "preact/hooks";
 
 export default function ThemeIconIsland() {
   useEffect(() => {
--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -1,9 +1,11 @@
-document.querySelector('.hamburger')?.addEventListener('click', () => {
-    const navLinks = document.querySelector(".nav-links")
-    navLinks?.classList.toggle('expanded');
-    const isExpanded = navLinks?.classList.contains('expanded')
-    
-    document.querySelectorAll('.hamburger .line').forEach(line => {
-        (line as HTMLElement).style.marginBottom = isExpanded ? '2px' : '10px';
-    })
-});
+document.addEventListener("astro:page-load", () => {
+    document.querySelector('.hamburger')?.addEventListener('click', () => {
+        const navLinks = document.querySelector(".nav-links")
+        navLinks?.classList.toggle('expanded');
+        const isExpanded = navLinks?.classList.contains('expanded')
+        
+        document.querySelectorAll('.hamburger .line').forEach(line => {
+            (line as HTMLElement).style.marginBottom = isExpanded ? '2px' : '10px';
+        })
+    });
+})
